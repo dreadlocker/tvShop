0c265f5c68d55dbe2dd038134ed968920045befa
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2019-04-15 14:48:31 +0300

    Server is uploaded

--- .history/server/models/tvs_20190415144438.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const tvSchema = new mongoose.Schema({
-   
-});
-
-const Tv = mongoose.model('Gig', tvSchema);
-
-module.exports = Tv;
--- .history/server/routes/tvs_20190415144413.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = app => {
-    app.get('/', (req, res) => {
-        
-    })
-}
