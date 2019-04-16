9a2bd03f358456b6add2af1b9dc8d2dd7609f104
Svetozar Iliev <svetozariliev13@gmail.com>
2019-04-16 09:51:44 +0300

    Additions to store page

--- tv-shop/src/App.vue
+++ tv-shop/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/store">Store</router-link> |
-      <router-link to="/promotions">Promotions</router-link> |
+      <router-link to="/">Home</router-link>|
+      <router-link to="/store">Store</router-link>|
+      <router-link to="/promotions">Promotions</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -11,14 +11,27 @@
 </template>
 
 <style lang="scss">
+html,
+body,
+p {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  // height: 100%;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  // margin: 0.5rem;
 }
 #nav {
+  // height: 2%;
   padding: 30px;
   a {
     font-weight: bold;
--- tv-shop/src/components/Store.vue
+++ tv-shop/src/components/Store.vue
@@ -1,29 +1,121 @@
 <template>
   <div class="store">
-    <h1>This is an STORE page</h1>
+    <div class="sideNav">
+      <div>
+        <p>МАРКА</p>
+        <hr>
+        <!-- <p class="right" v-for="name in namesArr" :key="name" :value="name">
+        <input v-model="checkedNames" type="checkbox" id="">{{name}}-->
+        <p class="right">
+          <input v-model="checkedNames" value="VORTEX" type="checkbox">VORTEX
+        </p>
+        <p class="right">
+          <input v-model="checkedNames" value="NEO" type="checkbox">
+          NEO
+        </p>
+        <p class="right">
+          <input v-model="checkedNames" value="LG" type="checkbox">
+          LG
+        </p>
+        <p class="right">
+          <input v-model="checkedNames" value="SAMSUNG" type="checkbox">
+          SAMSUNG
+        </p>
+        <p class="right">
+          <input v-model="checkedNames" value="SONY" type="checkbox">
+          SONY
+        </p>
+        <p class="right">
+          <input v-model="checkedNames" value="PHILIPS" type="checkbox">
+          PHILIPS
+        </p>
+        <!-- </p> -->
+      </div>
+    </div>
+    <div class="tvWrapper">
+      <div v-for="tvObj in tvsArr" :key="tvObj.id" class="singleTv">
+        <h3>{{tvObj.model}}</h3>
+        <img :src="tvObj.image">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'Store',
-}
+  name: "Store",
+  data() {
+    return {
+      tvsArr: [],
+      namesArr: ["VORTEX", "NEO", "LG", "SAMSUNG", "SONY", "PHILIPS"],
+      checkedNames: []
+    };
+  },
+  beforeMount() {
+    axios
+      .get("http://10.10.0.227:5432/tvs")
+      .then(response => {
+        this.tvsArr = response.data.tvs;
+      })
+      .catch(() => {
+        console.log(error);
+      });
+  },
+  mounted() {
+    // console.log(this.tvsArr);
+  },
+  watch: {
+    checkedNames: function(checked) {
+      axios
+        .post("http://10.10.0.227:5432/tvs", {
+          checked: checked
+        })
+        .then(response => {
+          this.tvsArr = response.data.tvs;
+          console.log(response.data.tvs);
+        })
+        .catch(() => {
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+
+<style lang="scss" scoped>
+.store {
+  display: flex;
+  justify-content: space-between;
+  // height: 92%;
 }
-ul {
-  list-style-type: none;
+.sideNav {
+  // width: 30%;
+  // height: 100%;
+  border: 1px solid red;
+}
+.tvWrapper {
+  display: flex;
+  // justify-content: space-between;
+  flex-wrap: wrap;
+  width: 67%;
+  // height: 100%;
+  border: 1px solid green;
+}
+.singleTv {
+  margin: 0;
   padding: 0;
+  border: 1px solid #cdcdcd;
+  // margin: 0.2rem;
+  // width: 32%;
+  @media only screen and (max-width: 1040px) {
+    // width: 100%;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.right {
+  text-align: left;
 }
 </style>
