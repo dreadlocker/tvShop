d13dd2116590ea4e1687a0901b8893c6258527fa
Svetozar Iliev <svetozariliev13@gmail.com>
2019-04-17 10:09:47 +0300

    Home, Store pages finished

--- tv-shop/src/components/Store.vue
+++ tv-shop/src/components/Store.vue
@@ -15,21 +15,11 @@ import { mapActions, mapState } from "vuex";
 import { ACTION_TVS_ARR } from "../Vuex/types.js";
 import axios from "axios";
 import BrandFilter from "./BrandFilter";
-// import * as requests from "../helper/requests.js";
-// import  requests  from "../mixins/requests.js";
 
 export default {
   name: "Store",
-  // mixins: [requests],
   components: {
     BrandFilter
-  },
-  data() {
-    return {
-      // tvsArr: [],
-      namesArr: ["VORTEX", "NEO", "LG", "SAMSUNG", "SONY", "PHILIPS"]
-      // checkedNames: []
-    };
   },
   computed: {
     ...mapState({
@@ -42,41 +32,10 @@ export default {
     })
   },
   mounted() {
-      // console.log(this.$requestHelper.getTvs());
-      // this.getRequest().then((res) => {
-      //   console.log(res);
-      // })
-
-      // console.log(this.getRequest());
-    // this.$requestHelper.getTvs().then(res => console.log(res));
-    // console.log(requests);
     axios
       .get("http://10.10.0.227:5432/tvs")
-      .then(response => {
-        // this.tvsArr = response.data.tvs;
-        this.tvs_arr_action(response.data.tvs);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  },
-  // mounted() {
-  // console.log(this.tvsArr);
-  // },
-  watch: {
-    checkedNames: function(checked) {
-      axios
-        .post("http://10.10.0.227:5432/tvs", {
-          checked: checked
-        })
-        .then(response => {
-          this.tvsArr = response.data.tvs;
-          console.log(response.data.tvs);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
+      .then(response => this.tvs_arr_action(response.data.tvs))
+      .catch(error => console.log(error));
   }
 };
 </script>
@@ -85,39 +44,27 @@ export default {
 .store {
   display: flex;
   justify-content: space-between;
-  // height: 92%;
 }
-// .sideNav {
-//   width: 7rem;
-//   // width: 30%;
-//   // height: 100%;
-//   border: 1px solid red;
-// }
 .tvWrapper {
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
   min-width: 200px;
-  // max-width: 92%;
   width: 90%;
-  // height: 100%;
   border: 1px solid green;
 }
 .singleTv {
   margin: 0;
   padding: 0;
   border: 1px solid #cdcdcd;
-  // margin: 0 0 1rem 0;
   width: 32%;
   &:hover {
     transform: scale(0.9);
   }
 
   @media only screen and (max-width: 1040px) {
+    // FIX za po malki ekrani
     // width: 100%;
   }
 }
-// .right {
-//   text-align: left;
-// }
 </style>
--- tv-shop/src/mixins/requests.js
+++ tv-shop/src/mixins/requests.js
@@ -1,13 +1,13 @@
-import { mapActions, mapState } from "vuex";
-import { ACTION_TVS_ARR } from "../Vuex/types";
+// import { mapActions, mapState } from "vuex";
+// import { ACTION_TVS_ARR } from "../Vuex/types";
 
-export default {
-  methods: {
-    getRequest() {
-      fetch('http://10.10.0.227:5432/tvs')
-        .then((response) => {
-          return response.json();
-        })
-    },
-  },
-};
+// export default {
+//   methods: {
+//     getRequest() {
+//       fetch('http://10.10.0.227:5432/tvs')
+//         .then((response) => {
+//           return response.json();
+//         })
+//     },
+//   },
+// };
