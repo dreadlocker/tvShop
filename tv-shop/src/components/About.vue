a54a818d4ed5cb26ed03242e1c7b65c372ab5aa3
Svetozar Iliev <svetozariliev13@gmail.com>
2019-04-17 09:50:52 +0300

    About page finished

--- tv-shop/src/components/About.vue
+++ tv-shop/src/components/About.vue
@@ -1,29 +1,61 @@
 <template>
-  <div class="about">
-    <h1>This is an ABOUT page</h1>
+  <div id="about">
+    <div v-for="(name, index) of creatorsArr" :key="index">
+      <h1>Contact {{name}}:</h1>
+      <div id="flex">
+        <a id="fb" :href="contactLinksObj[name][0]" target="_blank"></a>
+        <a id="gmail" :href="contactLinksObj[name][1]"></a>
+        <a id="skype" :href="contactLinksObj[name][2]" target="_blank"></a>
+      </div>
+      <hr v-if="index < creatorsArr.length - 1">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'About',
-}
+  name: "About",
+  data() {
+    return {
+      creatorsArr: ["Jivko Jelev", "Svetozar Iliev"],
+      contactLinksObj: {
+        "Jivko Jelev": [
+          "https://www.facebook.com/hankybrat",
+          "mailto:hankybrat1914@gmail.com",
+          "https://join.skype.com/invite/eobptMs9dzg3"
+        ],
+        "Svetozar Iliev": [
+          "https://www.facebook.com/neTbk.13Tu",
+          "mailto:svetozariliev13@gmail.com",
+          "https://join.skype.com/invite/otXS3Vmaa9Aj"
+        ]
+      }
+    };
+  }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+#flex {
+  display: flex;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#fb,
+#gmail,
+#skype {
+  margin: 0.5rem;
+  height: 3rem;
+  &:hover {
+    transform: scale(0.9);
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#fb {
+  content: url("../assets/facebook-logo.png");
 }
-a {
-  color: #42b983;
+#gmail {
+  content: url("../assets/gmail-logo.png");
+}
+#skype {
+  content: url("../assets/skype-icon.png");
 }
 </style>
