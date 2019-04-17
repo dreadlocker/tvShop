37a77ec8522b30fa1e5e6b1c3f3a776cad381b47
Svetozar Iliev <svetozariliev13@gmail.com>
2019-04-17 08:20:26 +0300

    Added animation on Home screen

---
+++ tv-shop/src/Vuex/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as types from './types'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tvsArr: [],
+    // current_page: []
+  },
+  mutations: {
+    [types.TVS_ARR]: (state, payload) => {
+      state.tvsArr = payload;
+    },
+  },
+  actions: {
+    [types.ACTION_TVS_ARR]({ commit }, payload) {
+      commit(types.TVS_ARR, payload);
+    },
+  }
+})
--- tv-shop/src/components/Store.vue
+++ tv-shop/src/components/Store.vue
@@ -1,39 +1,8 @@
 <template>
   <div class="store">
-    <div class="sideNav">
-      <div>
-        <p>МАРКА</p>
-        <hr>
-        <!-- <p class="right" v-for="name in namesArr" :key="name" :value="name">
-        <input v-model="checkedNames" type="checkbox" id="">{{name}}-->
-        <p class="right">
-          <input v-model="checkedNames" value="VORTEX" type="checkbox">VORTEX
-        </p>
-        <p class="right">
-          <input v-model="checkedNames" value="NEO" type="checkbox">
-          NEO
-        </p>
-        <p class="right">
-          <input v-model="checkedNames" value="LG" type="checkbox">
-          LG
-        </p>
-        <p class="right">
-          <input v-model="checkedNames" value="SAMSUNG" type="checkbox">
-          SAMSUNG
-        </p>
-        <p class="right">
-          <input v-model="checkedNames" value="SONY" type="checkbox">
-          SONY
-        </p>
-        <p class="right">
-          <input v-model="checkedNames" value="PHILIPS" type="checkbox">
-          PHILIPS
-        </p>
-        <!-- </p> -->
-      </div>
-    </div>
+    <BrandFilter/>
     <div class="tvWrapper">
-      <div v-for="tvObj in tvsArr" :key="tvObj.id" class="singleTv">
+      <div v-for="tvObj in tvs_arr" :key="tvObj.id" class="singleTv">
         <h3>{{tvObj.model}}</h3>
         <img :src="tvObj.image">
       </div>
@@ -42,30 +11,58 @@
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
+import { ACTION_TVS_ARR } from "../Vuex/types.js";
 import axios from "axios";
+import BrandFilter from "./BrandFilter";
+// import * as requests from "../helper/requests.js";
+import  requests  from "../mixins/requests.js";
 
 export default {
   name: "Store",
+  mixins: [requests],
+  components: {
+    BrandFilter
+  },
   data() {
     return {
-      tvsArr: [],
-      namesArr: ["VORTEX", "NEO", "LG", "SAMSUNG", "SONY", "PHILIPS"],
-      checkedNames: []
+      // tvsArr: [],
+      namesArr: ["VORTEX", "NEO", "LG", "SAMSUNG", "SONY", "PHILIPS"]
+      // checkedNames: []
     };
   },
-  beforeMount() {
-    axios
-      .get("http://10.10.0.227:5432/tvs")
-      .then(response => {
-        this.tvsArr = response.data.tvs;
-      })
-      .catch(() => {
-        console.log(error);
-      });
+  computed: {
+    ...mapState({
+      tvs_arr: state => state.tvsArr
+    })
   },
+  // methods: {
+  //   ...mapActions({
+  //     tvs_arr_action: ACTION_TVS_ARR
+  //   })
+  // },
   mounted() {
-    // console.log(this.tvsArr);
+      // console.log(this.$requestHelper.getTvs());
+      // this.getRequest().then((res) => {
+      //   console.log(res);
+      // })
+
+      // console.log(this.getRequest());
+    // this.$requestHelper.getTvs().then(res => console.log(res));
+    // console.log(requests);
+    // axios
+    //   .get("http://10.10.0.227:5432/tvs")
+    //   .then(response => {
+    //     // this.tvsArr = response.data.tvs;
+    //     this.tvs_arr_action(response.data.tvs);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
   },
+  // mounted() {
+  // console.log(this.tvsArr);
+  // },
   watch: {
     checkedNames: function(checked) {
       axios
@@ -76,7 +73,7 @@ export default {
           this.tvsArr = response.data.tvs;
           console.log(response.data.tvs);
         })
-        .catch(() => {
+        .catch(error => {
           console.log(error);
         });
     }
@@ -84,24 +81,25 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-
 <style lang="scss" scoped>
 .store {
   display: flex;
   justify-content: space-between;
   // height: 92%;
 }
-.sideNav {
-  // width: 30%;
-  // height: 100%;
-  border: 1px solid red;
-}
+// .sideNav {
+//   width: 7rem;
+//   // width: 30%;
+//   // height: 100%;
+//   border: 1px solid red;
+// }
 .tvWrapper {
   display: flex;
-  // justify-content: space-between;
+  justify-content: space-between;
   flex-wrap: wrap;
-  width: 67%;
+  min-width: 200px;
+  // max-width: 92%;
+  width: 90%;
   // height: 100%;
   border: 1px solid green;
 }
@@ -109,13 +107,17 @@ export default {
   margin: 0;
   padding: 0;
   border: 1px solid #cdcdcd;
-  // margin: 0.2rem;
-  // width: 32%;
+  // margin: 0 0 1rem 0;
+  width: 32%;
+  &:hover {
+    transform: scale(0.9);
+  }
+
   @media only screen and (max-width: 1040px) {
     // width: 100%;
   }
 }
-.right {
-  text-align: left;
-}
+// .right {
+//   text-align: left;
+// }
 </style>
