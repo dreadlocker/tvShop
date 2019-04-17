cbcf360ca530fc058f3eca3792239d8ab422de09
Svetozar Iliev <svetozariliev13@gmail.com>
2019-04-17 14:37:56 +0300

    Separate code in more components

---
+++ tv-shop/src/components/PageFromNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-view v-if="tvs_arr.length > 0"/>
+</template>
+
+<script>
+import axios from "axios";
+import { mapState, mapActions } from "vuex";
+import { ACTION_TVS_ARR } from "../Vuex/types.js";
+
+export default {
+  name: "Skeleton",
+  computed: {
+    ...mapState({
+      tvs_arr: state => state.tvsArr
+    })
+  },
+  methods: {
+    ...mapActions({
+      tvs_arr_action: ACTION_TVS_ARR
+    })
+  },
+  mounted() {
+    axios
+      .get("http://10.10.0.227:5432/tvs")
+      .then(res => this.tvs_arr_action(res.data.tvs))
+      .catch(err => console.log(err));
+  }
+};
+</script>
--- tv-shop/src/components/Skeleton.vue
+++ tv-shop/src/components/Skeleton.vue
@@ -1,50 +1,19 @@
 <template>
   <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/store">Store</router-link>|
-      <router-link to="/promotions">Promotions</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view v-if="tvs_arr.length > 0"/>
+    <Nav/>
+    <PageFromNav/>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import { mapState, mapActions } from "vuex";
-import { ACTION_TVS_ARR } from "../Vuex/types.js";
+import Nav from "./Nav";
+import PageFromNav from "./PageFromNav";
 
 export default {
   name: "Skeleton",
-  computed: {
-    ...mapState({
-      tvs_arr: state => state.tvsArr
-    })
-  },
-  methods: {
-    ...mapActions({
-      tvs_arr_action: ACTION_TVS_ARR
-    })
-  },
-  mounted() {
-    axios
-      .get("http://10.10.0.227:5432/tvs")
-      .then(res => this.tvs_arr_action(res.data.tvs))
-      .catch(err => console.log(err));
+  components: {
+    Nav,
+    PageFromNav
   }
 };
 </script>
-
-<style lang="scss" scoped>
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
